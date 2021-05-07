8a5e3f13cc638b8bb30830051b8401a45952a2b5
minimalScouser <sharukhpasha.lfc@gmail.com>
2021-05-07 20:45:35 +0530

    Add padding-left to placeholder

--- src/components/common/MapAutoComplete.jsx
+++ src/components/common/MapAutoComplete.jsx
@@ -1,19 +1,19 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 import GooglePlacesAutocomplete, {
   geocodeByPlaceId,
   geocodeByAddress,
   getLatLng,
-} from "react-google-places-autocomplete";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSearchLocation } from "@fortawesome/free-solid-svg-icons";
+} from 'react-google-places-autocomplete';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearchLocation } from '@fortawesome/free-solid-svg-icons';
 
 const API_KEY = process.env.REACT_APP_GMAPS_KEY;
 
 const MapAutoComplete = (props) => {
   const [value, setValue] = useState(null);
   const [state, setState] = useState({
-    lat: "",
-    lng: "",
+    lat: '',
+    lng: '',
   });
 
   useEffect(() => {
@@ -37,48 +37,45 @@ const MapAutoComplete = (props) => {
   }, [state]);
 
   return (
-    <>
-      <br />
-      <GooglePlacesAutocomplete
-        style={{ backgroundColor: "red" }}
-        apiKey={API_KEY}
-        selectProps={{
-          value,
-          onChange: setValue,
-          placeholder: (
-            <>
-              <FontAwesomeIcon icon={faSearchLocation} /> &nbsp;Search a
-              location...
-            </>
-          ),
-          styles: {
-            input: (provided) => ({
-              ...provided,
-              color: "#bbbfc8",
-              paddingLeft: '20px',
-            }),
-            // option: (provided) => ({
-            //   ...provided,
-            //   color: "red",
-            // }),
-            singleValue: (provided) => ({
-              ...provided,
-              color: "#bbbfc8",
-            }),
-            control: (provided) => ({
-              ...provided,
-              background: "#2c3942",
-              border: "0 !important",
-              // This line disable the blue border
-              boxShadow: "0 !important",
-              "&:hover": {
-                border: "0 !important",
-              },
-            }),
-          },
-        }}
-      />
-    </>
+    <GooglePlacesAutocomplete
+      style={{ backgroundColor: 'red' }}
+      apiKey={API_KEY}
+      selectProps={{
+        value,
+        onChange: setValue,
+        placeholder: (
+          <>
+            <FontAwesomeIcon icon={faSearchLocation} /> &nbsp;Search a
+            location...
+          </>
+        ),
+        styles: {
+          input: (provided) => ({
+            ...provided,
+            color: '#bbbfc8',
+          }),
+          // option: (provided) => ({
+          //   ...provided,
+          //   color: "red",
+          // }),
+          singleValue: (provided) => ({
+            ...provided,
+            color: '#bbbfc8',
+          }),
+          control: (provided) => ({
+            ...provided,
+            background: '#2c3942',
+            paddingLeft: '10px',
+            border: '0 !important',
+            // This line disable the blue border
+            boxShadow: '0 !important',
+            '&:hover': {
+              border: '0 !important',
+            },
+          }),
+        },
+      }}
+    />
   );
 };
 
