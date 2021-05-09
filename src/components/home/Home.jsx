795dc630f0bc45a45f75c3b84fbe5c0288dfd387
tmoynandy <tmoy.nandy@gmail.com>
2021-05-10 00:34:10 +0530

    added sort button

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,5 +1,7 @@
 import React, { Component, lazy, Suspense } from "react";
-
+import { Button } from "../common/RowColStyle";
+import { faSort } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const Cards = lazy(() => import("./cardView/Cards"));
 const MapAutoComplete = lazy(() => import("../common/MapAutoComplete"));
 const BadgeButtons = lazy(() => import("../common/BadgeButtons"));
@@ -19,6 +21,18 @@ export default class Home extends Component {
           <div class="column">
             <Suspense fallback={<></>}>
               <BadgeButtons />
+            </Suspense>
+          </div>
+        </div>
+        <div class="row">
+          <div
+            class="column"
+            style={{ marginLeft: "38%", marginBottom: "-1%" }}
+          >
+            <Suspense fallback={<></>}>
+              <Button color="#2c3942">
+                <FontAwesomeIcon icon={faSort} /> &nbsp;Sort
+              </Button>
             </Suspense>
           </div>
         </div>
